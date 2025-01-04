67a6e8441cbccd18bebe7bca01e2a78d977d165b
Maks Konnov <makkonn25@gmail.com>
2025-01-04 16:23:41 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+#создание нужных переменных
+username = 'имя пользователя:'
+title = 'заголовок:'
+content = 'описание:'
+status = 'статус:'
+created_date = '10-11-24'
+issue_date = '10-12-24'
+
+print('имя пользователя', username )
+print('заголовок:', title)
+print('описание:', content)
+print('статус:', status)
+print('10-11-24', created_date)
+print('10-12-24', issue_date)
