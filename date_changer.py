5856df3af212616d014496dd9643486b49cd4b9a
Maks Konnov <makkonn25@gmail.com>
2025-01-07 19:36:55 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,13 +1,12 @@
-username = input('Введите ваше имя: ')
-title = input('Заголовок  заметки: ')
-content = input('Описание хаметки: ')
-status = input('Укажите статус заметки (Активна/Неактивна): ')
-created_date = input('Дата создания заметки (в формате хх-хх-хх): ')
-issue_date = input('Дата истечения заметки (в формате хх-хх-хх): ')
-print('\nУказанные данные')
-print('Имя пользователя:', username)
-print('Заголовок:', title)
-print('Описание:', content)
-print('Статус:', status)
-print('Дата создания:', created_date [0:5])
-print('Дата истечения:', issue_date [0:5])
+username = 'райан гослинг'
+title = 'поход в магазин'
+content = 'купить пиво, кириешки и рыбку'
+status = 'Активна'
+created_date = '10-11-24'
+issue_date = '10-12-24'
+print('имя пользователя:', username )
+print('заголовок:', title)
+print('описание:', content)
+print('статус: ', status)
+print('дата создания заметки:', created_date[0:5])
+print('дата дедлайна:', issue_date[0:5])
